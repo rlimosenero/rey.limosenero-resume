6b29eeeefd6ee61da1d63d747db890c2aac74e81
rey.limosenero <rey.limosenero@gmail.com>
2025-11-04 23:38:52 +0800

    edit vite with the url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
+  base: mode === "production" ? "/rey.limosenero-resume/" : "/",
   server: {
     host: "::",
     port: 8080,
